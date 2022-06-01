dab919faa88aecfc109ccd39f6af3e3ea036c942
SpaceBeeGaming <space.bee.gaming@gmail.com>
2022-06-01 14:40:22 +0300

    Added XML Comments to StatModel.cs

--- AO3Statistics/StatModel.cs
+++ AO3Statistics/StatModel.cs
@@ -1,19 +1,64 @@
 ﻿namespace AO3Statistics;
 
+/// <summary>
+/// Data class representing work statistics.
+/// </summary>
 public class StatModel
 {
+    /// <summary>
+    /// Date and time of sampling.
+    /// </summary>
     public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Number of hits.
+    /// </summary>
     public int Hits { get; set; }
+
+    /// <summary>
+    /// Number of kudos on the fic.
+    /// </summary>
     public int Kudos { get; set; }
+
+    /// <summary>
+    /// Word count of the fic.
+    /// </summary>
     public int Words { get; set; }
+
+    /// <summary>
+    /// Number of published chapters.
+    /// </summary>
     public int Chapters { get; set; }
 
+    /// <summary>
+    /// Serializes the instance to a CSV format string./>
+    /// </summary>
+    /// <returns>CSV formatted string.</returns>
     public override string ToString()
         => String.Join(',', Date.ToString("O"), Kudos, Words, Chapters, Hits);
 
+    /// <summary>
+    /// Produces a formatted string representation.
+    /// </summary>
+    /// <param name="_">Value isn't relevant</param>
+    /// <returns>
+    /// A string formatted like:<br/>
+    /// <code>
+    /// "Date: <see cref="Date"/><br/>
+    /// Hits: <see cref="Hits"/><br/>
+    /// Kudos: <see cref="Kudos"/><br/>
+    /// Words: <see cref="Words"/><br/>
+    /// Chapters: <see cref="Chapters"/>"
+    /// </code>
+    /// </returns>
     public string ToString(bool _) =>
         $"Date: {Date}\nHits: {Hits}\nKudos: {Kudos}\nWords: {Words}\nChapters: {Chapters}";
 
+    /// <summary>
+    /// Deserializes a CSV formatted string into a <see cref="StatModel"/>.
+    /// </summary>
+    /// <param name="s">String to deserialize.</param>
+    /// <returns>A new <see cref="StatModel"/> filled with values from the string.</returns>
     public static StatModel FromString(string s)
     {
         var components = s.Split(',');
