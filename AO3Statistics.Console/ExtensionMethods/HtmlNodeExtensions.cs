8967b11dfcc2da32b00caaa18db4770990d0c1c6
SpaceBeeGaming <space.bee.gaming@gmail.com>
2023-11-14 22:12:16 +0200

    Documentation comments - HtmlNodeExtensions.cs - StringExtensions.cs - StatisticSnapshotModel.cs

--- AO3Statistics.Console/ExtensionMethods/HtmlNodeExtensions.cs
+++ AO3Statistics.Console/ExtensionMethods/HtmlNodeExtensions.cs
@@ -2,7 +2,19 @@
 
 namespace AO3Statistics.ConsoleApp.ExtensionMethods;
 
+/// <summary>
+/// Extension methods for <see cref="HtmlNode"/>.
+/// </summary>
 public static class HtmlNodeExtensions
 {
+
+    /// <summary>
+    /// <inheritdoc cref="HtmlNode.SelectSingleNode(string)"/>
+    /// </summary>
+    /// <remarks>This method has nullable annotations.</remarks>
+    /// <param name="node">The node to start from.</param>
+    /// <param name="xPath"><inheritdoc cref="HtmlNode.SelectSingleNode(string)"/></param>
+    /// <param name="_">A dummy to differentiate from the original.</param>
+    /// <returns><inheritdoc cref="HtmlNode.SelectSingleNode(string)"/></returns>
     public static HtmlNode? SelectSingleNode(this HtmlNode node, string xPath, bool _) => node.SelectSingleNode(xPath);
 }
--- AO3Statistics.Console/Models/StatisticsSnapshotModel.cs
+++ AO3Statistics.Console/Models/StatisticsSnapshotModel.cs
@@ -1,12 +1,31 @@
 ﻿using System.Text;
 
 namespace AO3Statistics.ConsoleApp.Models;
+
+/// <summary>
+/// Contains the combined user and work statistics.
+/// </summary>
 public sealed class StatisticsSnapshotModel
 {
+    /// <summary>
+    /// Date of creation.
+    /// </summary>
     public DateOnly Date { get; set; }
+    /// <summary>
+    /// A list containing the statistics for users works.
+    /// </summary>
     public required List<WorkStatisticsModel> WorkStatistics { get; init; }
+
+    /// <summary>
+    /// Contains the users statistics.
+    /// </summary>
     public required UserStatisticsModel UserStatistics { get; init; }
 
+    /// <summary>
+    /// Returns the combined user and work statistics in a pretty print format.
+    /// </summary>
+    /// <param name="_">A dummy to differentiate from other ToString() methods.</param>
+    /// <returns>A formatted string.</returns>
     public string ToString(bool _)
     {
         StringBuilder stringBuilder = new();
